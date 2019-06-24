4b6e33957f21061ec3a5f413a2b24c71e291ffaa
bhavin.coutloot <bhavin.coutloot@gmail.com>
2019-06-24 11:38:55 +0530

    Minor Changes

--- dep_file/app/models/template.js
+++
@@ -1,6 +0,0 @@
-const mongoose =require("mongoose");
-var Schema = mongoose.Schema;
-
-var templateSchema = new Schema({},{strict : false});
-
-module.exports=mongoose.model( "NS_test_tempEngine", templateSchema, "NS_test_tempEngine" );
--- dep_file/config/db.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    'url': 'mongodb://mahendra5338:avtkarh5d7Sann@68.183.82.76:27017/coutlootProducts'
-  };
